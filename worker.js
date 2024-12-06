8ea8f02d44132a2a469139fca9701cdbfef21726
SoniaM83 <Sonia Mesa>
2024-12-05 19:13:31 -0800

    updated Pokemon name

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 onmessage = function(event) {
     const pokemon= event.data;
 
-    const processedPokemon = pokemon.toUpperCase();
-    postMessage(processedPokemon);
+    const processedPokemon = pokemon.name.toUpperCase(); //CAPITALIZE THE POKEMON NAME
+    postMessage(processedPokemon); //SEND INFO TO MAIN THREAD
 };
